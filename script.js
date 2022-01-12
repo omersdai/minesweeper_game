8fcf15d583b206523ba70bea4e309bb25c625c1d
Omer Bey <omer.s.dai96@gmail.com>
2022-01-12 19:27:32 +0300

    about to implement placing mines and checking mines

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ const resetBtn = document.getElementById('resetBtn');
 // Minesweeper
 const minesweeperEl = document.getElementById('minesweeper');
 const difficultyEl = document.getElementById('difficulty');
+const flagCountEl = document.getElementById('flagCount');
+const clockEl = document.getElementById('clock');
+const flagHtml = '<i class="fas fa-flag fa-2x"></i>';
+const tick = 1000; // ms
 
 let difficulty;
+let gameActive;
 let flagRemaining;
+let interval;
 let time;
 let rows;
 
@@ -20,18 +26,21 @@ const difficulties = {
     colCount: 10,
     mineCount: 10,
     squareSize: 60, // px
+    fontSize: 20, //px
   },
   medium: {
     rowCount: 14,
     colCount: 18,
     mineCount: 40,
     squareSize: 45, // px
+    fontSize: 18, //px
   },
   hard: {
     rowCount: 20,
     colCount: 24,
     mineCount: 100,
     squareSize: 33, // px
+    fontSize: 13, //px
   },
 };
 
@@ -40,35 +49,90 @@ const [WON, CONTINUE, LOST] = ['won', 'continue', 'lost'];
 // Game
 function initializeGame() {
   difficulty = difficulties[difficultyEl.value];
+  gameActive = false;
   flagRemaining = difficulty.mineCount;
+  clearInterval(interval);
   time = 0;
 
   fillBoard();
 }
 
+function clickSquare(e) {
+  const square = e.target.tagName === 'DIV' ? e.target : e.target.parentNode;
+  if (square.classList.contains('active') || square.innerHTML !== '') return;
+
+  const row = parseInt(square.getAttribute('row'));
+  const col = parseInt(square.getAttribute('col'));
+  square.classList.add('active');
+
+  if (!gameActive) {
+    gameActive = true;
+    placeMines();
+  }
+
+  checkGameStatus();
+}
+
+function placeFlag(e) {
+  e.preventDefault();
+  const square = e.target.tagName === 'DIV' ? e.target : e.target.parentNode;
+  if (square.classList.contains('active')) return;
+
+  const row = parseInt(square.getAttribute('row'));
+  const col = parseInt(square.getAttribute('col'));
+  if (square.innerHTML === '') {
+    // place flag
+    square.innerHTML = flagHtml;
+    flagRemaining--;
+  } else {
+    // remove flag
+    square.innerHTML = '';
+    flagRemaining++;
+  }
+
+  flagCountEl.innerText = flagRemaining;
+  checkGameStatus();
+}
+
+function placeMines() {
+  interval = setInterval(() => {
+    time += tick;
+    updateClock();
+  }, tick);
+  console.log('placed mines');
+}
+
+function checkGameStatus() {
+  console.log('test');
+}
+
+function updateClock() {
+  const seconds = parseInt(time / 1000) % 60;
+  const minutes = parseInt(time / (1000 * 60));
+  clockEl.innerText = `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+}
+
 function fillBoard() {
-  const { rowCount, colCount, squareSize } = difficulty;
+  const { rowCount, colCount, squareSize, fontSize } = difficulty;
   minesweeperEl.innerHTML = ''; // clear board first
+  minesweeperEl.style.fontSize = `${fontSize}px`;
   for (let i = 0; i < rowCount; i++) {
     const row = document.createElement('div');
     row.className = 'row';
     for (let j = 0; j < colCount; j++) {
       const square = document.createElement('div');
       square.className = 'square';
+      if ((i + j) % 2 === 1) square.classList.add('dark');
       square.style.height = `${squareSize}px`;
       square.style.width = `${squareSize}px`;
+      square.setAttribute('row', i);
+      square.setAttribute('col', j);
+
       row.appendChild(square);
     }
-
     minesweeperEl.appendChild(row);
   }
-}
-
-function update() {
-  if (gameStatus === CONTINUE) {
-  } else {
-    showEndGame();
-  }
+  rows = minesweeperEl.querySelectorAll('.row');
 }
 
 function showEndGame() {
@@ -77,26 +141,13 @@ function showEndGame() {
   popupEl.classList.remove('hide');
 }
 
-document.addEventListener('keydown', (e) => {
-  switch (e.key) {
-    case RIGHT:
-      player.dx = player.speed;
-      break;
-    case LEFT:
-      player.dx = -player.speed;
-      break;
-  }
-});
-
-document.addEventListener('keyup', (e) => {
-  player.dx = 0;
-});
+minesweeperEl.addEventListener('click', clickSquare); // left click
+minesweeperEl.addEventListener('contextmenu', placeFlag); // right click
+difficultyEl.addEventListener('change', initializeGame); // change difficulty and reset game
 
 resetBtn.addEventListener('click', () => {
   popupEl.classList.add('hide');
   initializeGame();
 });
-
-difficultyEl.addEventListener('change', initializeGame);
 
 initializeGame();
